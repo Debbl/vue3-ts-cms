4d021778e260e60a07096f8f6c7ed224e6ee137f
Debbl <me@aiwan.run>
2022-02-18 21:26:13 +0800

    :lipstick: Add eslint prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80, // 打印宽度， 一行最长80个字符
+  tabWidth: 2, // 缩进的空格数
+  useTabs: false, // 缩进使用空格，不适用制表符
+  semi: true, // 语句末尾添加分号
+  singleQuote: true, // 使用单引号
+  trailingComma: 'es5', // 尾随逗号
+  bracketSpacing: true, // { foo: bar } 有空格
+};
