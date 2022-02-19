bd0fd70a8471cb5973c68f6d546100650b1d8d88
Debbl <me@aiwan.run>
2022-02-19 17:18:03 +0800

    feat: add element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { createApp } from 'vue';
 
 import router from './router';
 import store from './store';
+import 'element-plus/dist/index.css';
+
 import App from './App.vue';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount('#app');
